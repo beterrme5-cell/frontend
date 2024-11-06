2b6656a0fe5ecba0e5211b267879cce3de0fc444
Babar78 <muhammadbabarashraf8@gmail.com>
2024-11-07 00:16:34 +0500

    feat: implement custom 404 page and clean up App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import { Outlet } from "react-router-dom";
 function App() {
   return (
     <main className="App overflow-x-hidden">
-      <div className="fixed top-0 z-[10] flex">{/* Navbar Will be here */}</div>
-      <div className={`md:flex md:mt-[96px] mt-[72px] relative`}>
-        {/* Sidebar Component will be here */}
-        <Outlet />
-      </div>
+      {/* <div className="fixed top-0 z-[10] flex">Navbar Will be here</div> */}
+      {/* <div className={`md:flex md:mt-[96px] mt-[72px] relative`}> */}
+      {/* Sidebar Component will be here */}
+      <Outlet />
+      {/* </div> */}
     </main>
   );
 }
--- src/Router.jsx
+++ src/Router.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Link } from "react-router-dom";
 import App from "./App.jsx";
 import Dashboard from "./pages/dashboard/Dashboard";
 
@@ -12,8 +12,25 @@ export const router = createBrowserRouter([
         element: <Dashboard />,
       },
       {
-        path: "test",
-        element: <div>Test</div>,
+        path: "*",
+        element: (
+          <div className="flex justify-center items-center h-screen w-screen bg-slate-100">
+            <div className="flex flex-col justify-center gap-5 items-center">
+              <h1 className="text-5xl font-black text-darkBlue">
+                404 Not Found
+              </h1>
+              <p className="text-2xl text-gray-dark">
+                The page you are looking for does not exist.
+              </p>
+              <Link
+                href="/"
+                className="text-xl text-white bg-black px-4 py-2 rounded-[8px]"
+              >
+                Go back to home
+              </Link>
+            </div>
+          </div>
+        ),
       },
     ],
   },
