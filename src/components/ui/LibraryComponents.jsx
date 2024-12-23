572639a5bd654cafc87e53606cff8f2cf4ff2e41
Babar78 <muhammadbabarashraf8@gmail.com>
2024-12-24 02:26:31 +0500

    feat: added_firstName shortCode_button_in_toolbar_for_user_ease

--- src/components/ui/LibraryComponents.jsx
+++ src/components/ui/LibraryComponents.jsx
@@ -507,8 +507,24 @@ export const TextEditor = forwardRef(
       (state) => state.setTagsDropDownOpen
     );
 
+    const shortCodesSelected = useGlobalModals(
+      (state) => state.shortCodesSelected
+    );
+
+    const setShortCodesSelected = useGlobalModals(
+      (state) => state.setShortCodesSelected
+    );
+
     const containerRef = useRef(null);
     const onTextChangeRef = useRef(onTextChange);
+
+    const handleShortCodeGlobalState = (shortCode) => {
+      const updatedShortCodes = [...shortCodesSelected, shortCode];
+      // remove duplicates
+      const uniqueShortCodes = [...new Set(updatedShortCodes)];
+
+      setShortCodesSelected(uniqueShortCodes);
+    };
 
     useLayoutEffect(() => {
       onTextChangeRef.current = onTextChange;
@@ -548,8 +564,9 @@ export const TextEditor = forwardRef(
       customButtonsWrapper.id = "custom-buttons-wrapper";
 
       const pasteLinkButton = document.createElement("button");
-      pasteLinkButton.innerHTML = "Paste Video Link";
+      pasteLinkButton.innerHTML = `<div style="display: flex; align-items: center; gap: 4px; white-space: nowrap;"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" width="20" height="20"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z" fill="currentColor"/></svg><p style="margin: 0; line-height: normal;">Paste Video Link</p></div>`;
       pasteLinkButton.setAttribute("type", "button");
+
       pasteLinkButton.onclick = () => {
         // Paste the video link into the editor
         const range = quill.getSelection();
@@ -577,8 +594,9 @@ export const TextEditor = forwardRef(
       };
 
       const pasteThumbnailButton = document.createElement("button");
-      pasteThumbnailButton.innerHTML = "Paste Thumbnail";
+      pasteThumbnailButton.innerHTML = `<div style="display: flex; align-items: center; gap: 4px; white-space: nowrap;"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="18" height="18"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M448 80c8.8 0 16 7.2 16 16l0 319.8-5-6.5-136-176c-4.5-5.9-11.6-9.3-19-9.3s-14.4 3.4-19 9.3L202 340.7l-30.5-42.7C167 291.7 159.8 288 152 288s-15 3.7-19.5 10.1l-80 112L48 416.3l0-.3L48 96c0-8.8 7.2-16 16-16l384 0zM64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32zm80 192a48 48 0 1 0 0-96 48 48 0 1 0 0 96z" fill="currentColor"/></svg><p style="margin: 0; line-height: normal;">Paste Thumbnail</p></div>`;
       pasteThumbnailButton.setAttribute("type", "button");
+
       pasteThumbnailButton.onclick = () => {
         // Set the width and height
         const width = "300px"; // You can modify this to any value or make it dynamic
@@ -601,11 +619,35 @@ export const TextEditor = forwardRef(
         quill.setSelection(range.index + imageTag.length);
       };
 
+      const firstNameShortCodeBtn = document.createElement("button");
+      firstNameShortCodeBtn.innerHTML = `<div style="display: flex; align-items: center; gap: 4px; white-space: nowrap;"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"  width="18" height="18"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M192 128c0-17.7 14.3-32 32-32s32 14.3 32 32l0 7.8c0 27.7-2.4 55.3-7.1 82.5l-84.4 25.3c-40.6 12.2-68.4 49.6-68.4 92l0 71.9c0 40 32.5 72.5 72.5 72.5c26 0 50-13.9 62.9-36.5l13.9-24.3c26.8-47 46.5-97.7 58.4-150.5l94.4-28.3-12.5 37.5c-3.3 9.8-1.6 20.5 4.4 28.8s15.7 13.3 26 13.3l128 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-83.6 0 18-53.9c3.8-11.3 .9-23.8-7.4-32.4s-20.7-11.8-32.2-8.4L316.4 198.1c2.4-20.7 3.6-41.4 3.6-62.3l0-7.8c0-53-43-96-96-96s-96 43-96 96l0 32c0 17.7 14.3 32 32 32s32-14.3 32-32l0-32zm-9.2 177l49-14.7c-10.4 33.8-24.5 66.4-42.1 97.2l-13.9 24.3c-1.5 2.6-4.3 4.3-7.4 4.3c-4.7 0-8.5-3.8-8.5-8.5l0-71.9c0-14.1 9.3-26.6 22.8-30.7zM24 368c-13.3 0-24 10.7-24 24s10.7 24 24 24l40.3 0c-.2-2.8-.3-5.6-.3-8.5L64 368l-40 0zm592 48c13.3 0 24-10.7 24-24s-10.7-24-24-24l-310.1 0c-6.7 16.3-14.2 32.3-22.3 48L616 416z" fill="currentColor"/></svg><p style="margin: 0; line-height: normal;">Add First Name</p></div>`;
+      firstNameShortCodeBtn.setAttribute("type", "button");
+      firstNameShortCodeBtn.onclick = () => {
+        const firstNameShortCode = "{{first_name}}";
+        const range = quill.getSelection();
+        if (range === null) {
+          // Insert the image at the current cursor position
+          quill.clipboard.dangerouslyPasteHTML(0, firstNameShortCode);
+          // Move the cursor to the end of the inserted image
+          quill.setSelection(0 + firstNameShortCode.length);
+        } else {
+          // Insert the image at the current cursor position
+          quill.clipboard.dangerouslyPasteHTML(range.index, firstNameShortCode);
+          // Move the cursor to the end of the inserted image
+          quill.setSelection(range.index + firstNameShortCode.length);
+        }
+
+        // handle udpate the shortCodesSelected
+        handleShortCodeGlobalState(firstNameShortCode);
+      };
+
       // Style and append custom buttons to the wrapper
-      [pasteLinkButton, pasteThumbnailButton].forEach((btn) => {
-        btn.classList.add("ql-formats");
-        customButtonsWrapper.appendChild(btn);
-      });
+      [firstNameShortCodeBtn, pasteLinkButton, pasteThumbnailButton].forEach(
+        (btn) => {
+          btn.classList.add("ql-formats");
+          customButtonsWrapper.appendChild(btn);
+        }
+      );
 
       const tagsButtonWrapper = document.createElement("div");
       tagsButtonWrapper.id = "tag-button-wrapper";
@@ -791,16 +833,12 @@ const SelectContactShortCodeDropDown = ({ quillRef }) => {
     setShortCodesSelected(uniqueShortCodes);
 
     if (quillContent.ops.length === 1 && quillContent.ops[0].insert === "\n") {
-      quill.insertText(0, shortCodeValue, {
-        bold: true,
-      });
+      quill.insertText(0, shortCodeValue);
 
       quill.setSelection(textLength - 1 + shortCodeValue.length);
     } else {
       // Insert the Short Code with formatting
-      quill.insertText(textLength - 1, shortCodeValue, {
-        bold: true,
-      });
+      quill.insertText(textLength - 1, shortCodeValue);
 
       // Move the cursor to the end of the inserted Short Code
       quill.setSelection(textLength + shortCodeValue.length);
