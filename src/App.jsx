aba64e28b493e3ac1e6e76dffb4d2cc737e7b6fb
Babar78 <muhammadbabarashraf8@gmail.com>
2025-02-06 23:27:34 +0500

    feat: update_modal_rendering_logic_and_moved_them_into_Appjs

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import { useGlobalModals } from "./store/globalModals";
 import {
   ContactsSelectionModalEmail,
   ContactsSelectionModalSMS,
+  DeleteVideoConfirmationModal,
+  EditVideoModal,
+  PreRecordingDataInputModal,
+  ShareVideoModal,
+  StartRecordingWarningModal,
+  UpdateUserDomainModal,
 } from "./components/ui/GlobalModals";
 
 function App() {
@@ -14,10 +20,34 @@ function App() {
     (state) => state.isContactsSelectionModalOpen
   );
 
+  const isShareVideoModalOpen = useGlobalModals(
+    (state) => state.isShareVideoModalOpen
+  );
+  const isEditVideoModalOpen = useGlobalModals(
+    (state) => state.isEditVideoModalOpen
+  );
+  const isDeleteVideoModalOpen = useGlobalModals(
+    (state) => state.isDeleteVideoModalOpen
+  );
+  const updateDomainModalOpen = useGlobalModals(
+    (state) => state.updateDomainModalOpen
+  );
+  const isWarningModalOpen = useGlobalModals(
+    (state) => state.isWarningModalOpen
+  );
+  const isNewRecordingModalOpen = useGlobalModals(
+    (state) => state.isNewRecordingModalOpen
+  );
   return (
     <main className="App overflow-x-hidden md:p-[32px] p-[20px]">
       {isContactsSelectionModalOpen && <ContactsSelectionModalEmail />}
       {isSMSContactsSelectionModalOpen && <ContactsSelectionModalSMS />}
+      {isShareVideoModalOpen && <ShareVideoModal />}
+      {isEditVideoModalOpen && <EditVideoModal />}
+      {isDeleteVideoModalOpen && <DeleteVideoConfirmationModal />}
+      {updateDomainModalOpen && <UpdateUserDomainModal />}
+      {isWarningModalOpen && <StartRecordingWarningModal />}
+      {isNewRecordingModalOpen && <PreRecordingDataInputModal />}
       <Outlet />
     </main>
   );
