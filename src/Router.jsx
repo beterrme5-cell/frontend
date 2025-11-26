331792f7a946fea4017b0e1fed09adff698d864c
beterrme5-cell <beterrme5@gmail.com>
2025-11-26 19:29:16 +0500

    frontend route

--- src/Router.jsx
+++ src/Router.jsx
@@ -51,7 +51,7 @@ export const router = createBrowserRouter([
         element: <Test />,
       },
       {
-        path: "/d",
+        path: "/dashboard-test",
         element: <D />,
       },
       {
