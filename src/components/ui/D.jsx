55b82ee0c072b91eebff29e3b7b84d54cfee8aa0
beterrme5-cell <beterrme5@gmail.com>
2025-12-03 04:41:20 +0500

    Update D.jsx

--- src/components/ui/D.jsx
+++ src/components/ui/D.jsx
@@ -5,6 +5,9 @@ import { HiViewGrid, HiViewList } from "react-icons/hi";
 import { FiGrid } from "react-icons/fi";
 
 import { LuTrendingUp } from "react-icons/lu";
+import VideoRecorder from "./VideoRecorder";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 function D() {
   return (
@@ -32,7 +35,7 @@ function D() {
           {/* Three Cards */}
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-16">
             {/* Card 1 - Create New Video */}
-            <div className="bg-white rounded-xl flex flex-col items-center p-8 shadow-lg border border-gray-200 hover:shadow-xl transition-shadow">
+            {/* <div className="bg-white rounded-xl flex flex-col items-center p-8 shadow-lg border border-gray-200 hover:shadow-xl transition-shadow">
               <div className="border rounded-lg p-4 w-16 h-16 flex items-center justify-center mb-6 icon-container-glow">
                 <FiVideo className="text-primary" size={28} />
               </div>
@@ -50,7 +53,8 @@ function D() {
                   editing tools
                 </p>
               </div>
-            </div>
+            </div> */}
+            <VideoRecorder />
 
             {/* Card 2 - Video Library */}
             <div className="bg-white rounded-xl flex flex-col items-center p-8 shadow-lg border border-gray-200 hover:shadow-xl transition-shadow">
